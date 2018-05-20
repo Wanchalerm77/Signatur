26e6ec51554cb3bb7688e03863e56d1aadd2c6e7
Wanchalerm Honghom <wanhonghom@online.de>
2018-05-20 10:52:28 +0200

    yes

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,6 @@
 import "normalize.css";
 import "../sass/main.sass";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "clipboard/dist/clipboard.min";
+
+let clipboard = new ClipboardJS(".btn");
